6b60b0beb65a4e0a1b5bed4b5dafa6573045b9d9
hha00m <hayder_ok@yahoo.com>
2020-09-04 23:04:11 +0300

    bugs

---
+++ build/precache-manifest.d09cc549aefea0ab37e5f22d79f73b2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b144cc820dc4ce166041cdcf2e255dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5717da613d7dc83b92f8",
+    "url": "/static/css/2.f3d938b9.chunk.css"
+  },
+  {
+    "revision": "69884e691c921b35fe3e",
+    "url": "/static/css/main.47cdc0d7.chunk.css"
+  },
+  {
+    "revision": "5717da613d7dc83b92f8",
+    "url": "/static/js/2.e7e66dc9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.e7e66dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69884e691c921b35fe3e",
+    "url": "/static/js/main.f1828118.chunk.js"
+  },
+  {
+    "revision": "7df953784abf4a2a770b",
+    "url": "/static/js/runtime-main.c0670b83.js"
+  }
+]);
